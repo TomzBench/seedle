5895d80cf578c13c2b3e41a9e0c72577ff5a27b0
thomas <thomas.chiantia@gmail.com>
2023-08-14 14:54:51 -0400

    fixed collect filter to use key instead of array

--- gen/src/filters/collect.rs
+++ gen/src/filters/collect.rs
@@ -3,7 +3,8 @@ use liquid_core::{
     Display_filter, Filter, FilterParameters, FilterReflection, FromFilterParameters, ParseFilter,
 };
 use liquid_core::{Expression, Result, Runtime};
-use liquid_core::{Value, ValueView};
+use liquid_core::{Object, Value, ValueView};
+use std::collections::BTreeMap;
 
 // TODO Input is of tyep Value, arguments are of type Expression
 //      Therefore we need to impl From<LinkedKeyVal> for Value
@@ -37,19 +38,20 @@ impl Filter for CollectFilter {
             .as_scalar()
             .ok_or_else(|| invalid_argument("name", "string expected"))?
             .into_cow_str();
-        let obj = input
+        Ok(input
             .as_object()
-            .ok_or_else(|| invalid_input("Object expected"))?;
-        let values = obj
-            .values()
-            .filter_map(|val| {
-                let inner = val.as_object()?;
-                match inner.get("type")?.as_scalar()?.into_cow_str().as_ref() {
-                    n if n == name => Some(inner.to_value()),
-                    _ => None,
-                }
-            })
-            .collect::<Vec<Value>>();
-        Ok(Value::Array(values))
+            .ok_or_else(|| invalid_input("Object expected"))?
+            .iter()
+            .filter_map(|(key, val)| is_type(&name, val).map(|inner| (key, inner)))
+            .collect::<BTreeMap<_, Value>>()
+            .to_value())
+    }
+}
+
+fn is_type(ty: &str, val: &dyn liquid_core::ValueView) -> Option<Value> {
+    let inner = val.as_object()?;
+    match inner.get("type")?.as_scalar()?.into_cow_str().as_ref() {
+        n if n == ty => Some(inner.to_value()),
+        _ => None,
     }
 }
--- gen/src/filters/tests/collect.rs
+++ gen/src/filters/tests/collect.rs
@@ -2,8 +2,8 @@ use crate::filters::collect::Collect;
 use liquid_core::{Object, Value};
 use seedle_parser::{ConstrainedPrimative, Fields, LinkedKeyVal, LinkedNode, Literal};
 
-// TODO 
-//      - Collect should return an object instead of an array so we can preserve 
+// TODO
+//      - Collect should return an object instead of an array so we can preserve
 //        the keys
 #[test]
 fn expect_collect() {
@@ -20,16 +20,18 @@ fn expect_collect() {
             state
         },
     );
-    let expect_literals = Value::Array(vec![Value::from(LinkedNode::Literal(Literal::UInt(3)))]);
-    let expect_primatives = Value::Array(vec![Value::from(LinkedNode::Primative(
-        ConstrainedPrimative::Bool,
-    ))]);
-    let expect_structs = Value::Array(vec![Value::from(LinkedNode::Struct(Fields {
-        members: vec![
-            LinkedKeyVal::new("address", ConstrainedPrimative::Str(16).into()),
-            LinkedKeyVal::new("port", ConstrainedPrimative::U16.into()),
+    let expect_literals = Value::Object(liquid_core::object!({
+        "l0": Value::from(LinkedNode::Literal(Literal::UInt(3)))
+    }));
+    let expect_primatives = Value::Object(liquid_core::object!({
+        "p0": Value::from(LinkedNode::Primative(ConstrainedPrimative::Bool))
+    }));
+    let expect_structs = Value::Object(liquid_core::object!({
+        "s0": Value::from(LinkedNode::Struct(Fields {members: vec![
+              LinkedKeyVal::new("address", ConstrainedPrimative::Str(16).into()),
+              LinkedKeyVal::new("port", ConstrainedPrimative::U16.into()),
         ],
-    }))]);
+    }))}));
     assert_eq!(
         expect_literals,
         liquid_core::call_filter!(Collect, input, "literal").unwrap()
